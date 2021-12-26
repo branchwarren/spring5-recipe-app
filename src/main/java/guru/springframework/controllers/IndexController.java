dae33b61746277fa3447328988c6f9a23d4951ec
Alessandro Pace <alessandro.pace@h-farm.com>
2021-12-26 18:58:02 +0100

    Commit for less 141 - Spring Data JPA Query Methods

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -1,7 +1,13 @@
 package guru.springframework.controllers;
 
+import guru.springframework.domain.Category;
+import guru.springframework.domain.UnitOfMeasure;
+import guru.springframework.repository.CategoryRepository;
+import guru.springframework.repository.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 /**
  * Created by jt on 6/1/17.
@@ -9,8 +15,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
 
+    private CategoryRepository categoryRepository;
+    private UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage(){
+
+        Optional<Category> optionalCategory = categoryRepository.findByDescription("American");
+        Optional<UnitOfMeasure> optionalUnitOfMeasure = unitOfMeasureRepository.findByDescription("Teaspoon");
+
+        System.out.println("Category Id is: " + optionalCategory.get().getId());
+        System.out.println("UOM Id is: " + optionalUnitOfMeasure.get().getId());
+
         return "index";
     }
 }
